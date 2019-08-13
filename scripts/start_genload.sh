795a713be1bf6153a62e67c38e48055e639d59fe
lokey <lokeshiitm08@gmail.com>
2019-08-13 14:49:57 +0530

    Update genload start service code.

--- scripts/start_genload.sh
+++ scripts/start_genload.sh
@@ -1,4 +1,5 @@
 chown -R ubuntu:ubuntu /home/ubuntu/rudder-server
 cd /home/ubuntu/rudder-server/tests/load
 echo "Ready to run genload.go"
-go run genload.go
+systemctl enable rudder-genload.service
+systemctl restart rudder-genload.service
