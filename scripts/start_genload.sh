117e61f7ee6a9c9ae57d97d77ef872904e69cf26
lokey <lokeshiitm08@gmail.com>
2019-08-13 15:25:35 +0530

    Move appspec file inside loads test folder.

--- scripts/start_genload.sh
+++
@@ -1,5 +0,0 @@
-chown -R ubuntu:ubuntu /home/ubuntu/rudder-server
-cd /home/ubuntu/rudder-server/tests/load
-echo "Ready to run genload.go"
-systemctl enable rudder-genload.service
-systemctl restart rudder-genload.service
